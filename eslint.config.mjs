a56a6c556291d0e3302f3b2bcf5a436cdb49f16a
Artem Godin <130038316+124c4a@users.noreply.github.com>
2025-06-14 01:03:23 +0300

    chore: include Apache License file to the project
    Signed-off-by: Artem Godin <130038316+124c4a@users.noreply.github.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import nx from '@nx/eslint-plugin';
 import perfectionist from 'eslint-plugin-perfectionist';
+import headers from 'eslint-plugin-headers';
 
 export default [
   ...nx.configs['flat/base'],
@@ -33,6 +34,7 @@ export default [
   {
     plugins: {
       perfectionist,
+      headers,
     },
   },
   {
@@ -53,6 +55,33 @@ export default [
         {
           type: 'natural',
           order: 'asc',
+        },
+      ],
+      'headers/header-format': [
+        'error',
+        {
+          style: 'jsdoc',
+          blockPrefix: '\n',
+          blockSuffix: '\n',
+          linePrefix: ' ',
+          source: 'string',
+          content: `Copyright {year} {author}.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.`,
+          variables: {
+            year: String(new Date().getFullYear()),
+            author: 'Artem Godin',
+          },
         },
       ],
     },
